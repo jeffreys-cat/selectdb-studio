1c63e9e8604dee9d94f43f218b56248891f4fc3a
Jeffrey <color.dove@gmail.com>
2025-03-05 16:55:14 +0800

    feat: update

--- components/get-started.tsx
+++ components/get-started.tsx
@@ -4,13 +4,14 @@ import { useToast } from "@/hooks/use-toast";
 import ShinyButton from "./magicui/shiny-button";
 import { page_routes } from "@/lib/routes-config";
 import Link from "next/link";
+import { downloadUrl } from "@/lib/app-config";
 
 export function GET() {
   const { toast } = useToast();
 
   return (
     <Link
-      href={`https://test-cdn-selectdb.oss-cn-beijing.aliyuncs.com/selectdb-studio/SelectDB%20Studio-1.0.0-alpha.24-arm64.dmg`}
+      href={downloadUrl}
       download
     >
       <ShinyButton>下载 Mac Alpha</ShinyButton>
